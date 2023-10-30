ffb4e902893bb090d1d77af68de9935a1b36d7c7
Michael Klein <Michaelklein1997@gmail.com>
2023-10-30 13:37:12 -0500

    initial commit

---
+++ src/main/java/com/company/fantasyturnedreal/FantasyTurnedRealApplication.java
@@ -0,0 +1,13 @@
+package com.company.fantasyturnedreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTurnedRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTurnedRealApplication.class, args);
+	}
+
+}
