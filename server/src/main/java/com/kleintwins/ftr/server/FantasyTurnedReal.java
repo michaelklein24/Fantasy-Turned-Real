98e460ecffcab0ab7493cb876e3307d22accd12f
Michael Klein <michaelklein1997@gmail.com>
2025-01-02 16:44:54 -0600

    initial project set up

---
+++ server/src/main/java/com/kleintwins/ftr/server/FantasyTurnedReal.java
@@ -0,0 +1,22 @@
+package com.kleintwins.ftr.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import static com.kleintwins.ftr.server.FantasyTurnedReal.basePackage;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {basePackage})
+@EnableJpaRepositories(basePackage)
+@EntityScan(basePackage)
+public class FantasyTurnedReal {
+
+    public final static String basePackage = "com.kleintwins.ftr";
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyTurnedReal.class, args);
+    }
+}
