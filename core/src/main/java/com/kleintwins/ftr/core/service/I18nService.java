42b2bc97c1c974e389ce832436223ed136537125
Michael Klein <michaelklein1997@gmail.com>
2025-01-02 16:45:26 -0600

    created i18n service

---
+++ core/src/main/java/com/kleintwins/ftr/core/service/I18nService.java
@@ -0,0 +1,26 @@
+package com.kleintwins.ftr.core.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class I18nService {
+    private final MessageSource messageSource;
+
+    public String translate(String key) {
+        return translate(key, null);
+    }
+
+    public String translate(String key, String ...args) {
+        try {
+            return messageSource.getMessage(key, args, LocaleContextHolder.getLocale());
+        } catch (NoSuchMessageException e) {
+            return "<MissingTranslation key: " + key + " />";
+        }
+    }
+}
+
