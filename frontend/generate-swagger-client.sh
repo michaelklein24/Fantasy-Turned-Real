a9d51783e55e50ecef19b036d0f979c8497d7715
Michael Klein <michaelklein1997@gmail.com>
2025-01-06 12:10:04 -0600

    updated generated library name

--- frontend/generate-swagger-client.sh
+++ frontend/generate-swagger-client.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-DIRECTORY=""
+DIRECTORY="./src/app/shared/generated"  # Use a relative path to your local directory
 
-# Check if the directory exists
+# Ensure the directory is in your local file system
 if [ -d "$DIRECTORY" ]; then
   echo "Deleting directory: $DIRECTORY"
   rm -rf "$DIRECTORY"
 fi
-echo "Making directory: "$DIRECTORY""
+echo "Making directory: $DIRECTORY"
 mkdir -p "$DIRECTORY"
 
+# Run the Swagger Codegen to generate the library
 docker run --rm \
   -v $(pwd):/local \
   swaggerapi/swagger-codegen-cli-v3 generate \
   -i http://host.docker.internal:8080/v3/api-docs \
   -l typescript-axios \
-  -o /local/src/app/shared/api
+  -o /local/src/app/shared/generated \
