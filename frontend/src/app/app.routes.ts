8bf7b9b4c763db5a13a0c9182a27eed153ca780e
Michael Klein <michaelklein1997@gmail.com>
2025-01-06 08:49:35 -0600

    added navbar with routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterUserFormComponent } from './forms/register-user-form/register-user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "register", component: RegisterUserFormComponent}
+];
