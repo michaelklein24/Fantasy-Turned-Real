11070d75df26b9c1b3c9fff8f23b5fd4ea12142c
Michael Klein <michaelklein1997@gmail.com>
2025-01-07 06:41:33 -0600

    added dashboard component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterUserFormComponent } from './forms/register-user-form/register-user-form.component';
 import { LoginUserFormComponent } from './forms/login-user-form/login-user-form.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: "register", component: RegisterUserFormComponent},
-    { path: "login", component: LoginUserFormComponent}
+    { path: "login", component: LoginUserFormComponent},
+    { path: "dashboard", component: DashboardComponent }
 ];
