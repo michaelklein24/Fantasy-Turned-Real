b6a9b4b50174caa7e4772ffb005df37fb388227c
Michael Klein <michaelklein1997@gmail.com>
2025-01-08 09:17:23 -0600

    updated request matchers

--- backend/auth/src/main/java/com/kleintwins/ftr/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/com/kleintwins/ftr/auth/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/auth/register", "v3/api-docs").permitAll()
+                        .requestMatchers("/auth/**", "v3/api-docs").permitAll()
                         .anyRequest().authenticated());
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
